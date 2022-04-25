a598d299615f5ff6d20413134883a46bac2eea68
FredrickWambua <fredrickswambua254@gmail.com>
2022-04-25 15:56:53 +0300

    adds form validation method

--- to-do/app.ts
+++ to-do/app.ts
@@ -1,16 +1,34 @@
+
+
+const toDoList = document.getElementById('todo-list')
+class ToDo {
+    constructor(
+        public id: number,
+        public title: string | number,
+        public description: string , 
+        public status: ToDoStatus,
+        public message: string,
+        ){}
+}
+
 enum ToDoStatus{
     incomplete,
     completed,
 }
 
-class ToDo {
-    constructor(
-        public id: number,
-        public title: string, 
-        public description: string , 
-        public status: ToDoStatus,
-        public message: string
-        ){}
+interface ValidateData {
+    value: string | number;
+    required: true;
+
+}
+
+function validate(validateInput: ValidateData): boolean{
+    let isValid = true;
+
+    if (validateInput.required){
+        isValid = isValid && validateInput.value.toString().trim().length !==0;
+    }
+    return isValid;
 }
 
 class ToDoInput {
@@ -26,26 +44,48 @@ class ToDoInput {
         this.message = document.getElementById('message') as HTMLElement;
 
         this.getInputData();
+        this.acceptData();
     }
     private getInputData(){
         this.formElem.addEventListener('submit', (e) =>{
             e.preventDefault();
-            console.log('button clicked')
-            const title = this.titleElem.value;
-            const description = this.descriptionElem.value;
-            console.log(title, description)
+            const userData = this.acceptData();
+
+            if (userData){
+                console.log(userData)
+                this.clearInput();
+            }
 
             
         });
-        this.formValidation()
     }
-    private formValidation = ()=>{
-        if (this.titleElem.value === '' && this.descriptionElem.value === ''){
-            this.message.innerHTML = 'All input fields must not be blank!';
-        } else {
-            this.message.innerHTML = '';
+    private clearInput(){
+        this.titleElem.value = '';
+        this.descriptionElem.value = '';
+    }
+    private acceptData(): [string | number, string] | void{
+        const title = this.titleElem.value;
+        const description = this.descriptionElem.value;
+
+        const titleValidate: ValidateData = {
+            value: title, 
+            required: true,
+        };
+
+        const descriptionValidate: ValidateData = {
+            value: description,
+            required: true,
         }
-    };
+        if (!validate(titleValidate) || !validate(descriptionValidate)){
+            this.message.innerHTML = 'Fields cannot be empty!'
+            
+        }else{
+            this.message.innerHTML = ''
+
+        }
+        return [title, description];
+    }
+
 }
 
 const ToDoInstance = new ToDoInput() ;
--- to-do/dist/app.js
+++ to-do/dist/app.js
@@ -1,9 +1,5 @@
 "use strict";
-var ToDoStatus;
-(function (ToDoStatus) {
-    ToDoStatus[ToDoStatus["incomplete"] = 0] = "incomplete";
-    ToDoStatus[ToDoStatus["completed"] = 1] = "completed";
-})(ToDoStatus || (ToDoStatus = {}));
+const toDoList = document.getElementById('todo-list');
 class ToDo {
     constructor(id, title, description, status, message) {
         this.id = id;
@@ -13,31 +9,59 @@ class ToDo {
         this.message = message;
     }
 }
+var ToDoStatus;
+(function (ToDoStatus) {
+    ToDoStatus[ToDoStatus["incomplete"] = 0] = "incomplete";
+    ToDoStatus[ToDoStatus["completed"] = 1] = "completed";
+})(ToDoStatus || (ToDoStatus = {}));
+function validate(validateInput) {
+    let isValid = true;
+    if (validateInput.required) {
+        isValid = isValid && validateInput.value.toString().trim().length !== 0;
+    }
+    return isValid;
+}
 class ToDoInput {
     constructor() {
-        this.formValidation = () => {
-            if (this.titleElem.value === '' && this.descriptionElem.value === '') {
-                this.message.innerHTML = 'All input fields must not be blank!';
-            }
-            else {
-                this.message.innerHTML = '';
-            }
-        };
         this.formElem = document.querySelector('form');
         this.titleElem = document.getElementById('title');
         this.descriptionElem = document.getElementById('description');
         this.message = document.getElementById('message');
         this.getInputData();
+        this.acceptData();
     }
     getInputData() {
         this.formElem.addEventListener('submit', (e) => {
             e.preventDefault();
-            console.log('button clicked');
-            const title = this.titleElem.value;
-            const description = this.descriptionElem.value;
-            console.log(title, description);
+            const userData = this.acceptData();
+            if (userData) {
+                console.log(userData);
+                this.clearInput();
+            }
         });
-        this.formValidation();
+    }
+    clearInput() {
+        this.titleElem.value = '';
+        this.descriptionElem.value = '';
+    }
+    acceptData() {
+        const title = this.titleElem.value;
+        const description = this.descriptionElem.value;
+        const titleValidate = {
+            value: title,
+            required: true,
+        };
+        const descriptionValidate = {
+            value: description,
+            required: true,
+        };
+        if (!validate(titleValidate) || !validate(descriptionValidate)) {
+            this.message.innerHTML = 'Fields cannot be empty!';
+        }
+        else {
+            this.message.innerHTML = '';
+        }
+        return [title, description];
     }
 }
 const ToDoInstance = new ToDoInput();
